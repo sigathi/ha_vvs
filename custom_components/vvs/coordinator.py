8b294d6990e4b7808f0d0db9a910b99db1dd09ab
sigathi <s.thi@t-online.de>
2025-11-24 20:28:42 +0100

    Update coordinator.py

--- custom_components/vvs/coordinator.py
+++ custom_components/vvs/coordinator.py
@@ -8,6 +8,13 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 from homeassistant.util import dt as dt_util
 
+import os
+import sys
+
+current_path = os.path.dirname(__file__)
+if current_path not in sys.path:
+    sys.path.append(current_path)
+
 import vvspy
 from .vvspy.enums.stations import Station
 from .const import SCAN_INTERVAL
